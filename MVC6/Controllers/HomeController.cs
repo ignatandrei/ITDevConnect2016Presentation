7491ee0e1f2bdc603c664aa4607612a0f41d6ec1
Andrei Ignat <ignatandrei@yahoo.com>
2016-04-26 08:28:40 +0300

    MVC6

---
+++ MVC6/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using AMVC6.Models;
+
+namespace AMVC6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IPerson _p;
+        public HomeController(IPerson p)
+        {
+            _p = p;
+        }
+        public IActionResult Index()
+        { 
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var p = new Person();
+            p.id = id;
+            p.Name = " nume " + id;
+            return View(p);
+        }
+        public IActionResult Verif()
+        {
+            return Content(" the person " + _p.Name);
+        }
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC6/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMVC6.Models
+{
+    public interface IPerson
+    {
+        int id { get; set; }
+        string Name { get; set; } 
+    }
+    public class Person:IPerson
+    {
+        public int id { get; set; }
+        public string Name { get; set; } = "Andrei";
+    }
+}
