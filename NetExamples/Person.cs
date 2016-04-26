0f088a883967287468f6a3c3e1755e9604635a27
Andrei Ignat <ignatandrei@yahoo.com>
2016-04-26 08:25:53 +0300

    Added files via upload

---
+++ NetExamples/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetExamples
+{
+
+    public class Person
+    {
+        public static int MinAgeForDriving{get;}=18;
+
+        public int MaxAge{get;private set;}=200;
+        
+        public int Age{get;set;}
+        public string Name{get;set;}
+
+        internal string ToDebug()
+        {
+          
+            var q=  $"Name = {Name:0}";  
+            return q; 
+        }
+        public void LogMethod(int x13){
+            System.Console.WriteLine(
+                $"in method {nameof(LogMethod)} with argument {nameof(x13)} value {x13}"
+                );
+        }
+        public bool HasRightToDrive=>Age>=18;
+        public bool HasRightToDrive1()=>Age>=18;
+        public void ExceptionForMonday(){
+            try{
+                int x=1;
+                x--;
+                x=x/x;
+            }
+            catch (Exception e) when (
+              (DateTime.Now.DayOfWeek == DayOfWeek.Saturday)
+              || (DateTime.Now.DayOfWeek == DayOfWeek.Sunday
+)) {
+            //just log for monday
+            }
+    }
+            
+   }
+}
---
+++ NetExamples/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static System.ConsoleColor;
+namespace NetExamples
+{
+    /// <summary>
+    /// https://msdn.microsoft.com/en-us/magazine/dn879355.aspx
+    /// https://sachabarbs.wordpress.com/2016/04/19/entity-framework-7-in-memory-provider-test/
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var p = new Person();
+            p.Name = "Andrei";
+            //auto property
+            Console.WriteLine(p.MaxAge);
+            //string.format
+            Console.WriteLine($"The age is {p.MaxAge}");
+            //string format inside
+            Console.WriteLine(p.ToDebug());
+            //nameof
+            p.LogMethod(3);
+            //exception when
+            p.ExceptionForMonday();
+            //static using
+            var c = Console.ForegroundColor;
+            Console.ForegroundColor = Red;
+            Console.WriteLine("using static System.ConsoleColor");
+            Console.ForegroundColor = c;
+            //expression bodies
+            Console.WriteLine(p.HasRightToDrive);
+            Console.WriteLine(p.HasRightToDrive1());
+            //null conditional operator
+            p = null;
+            bool? x = p?.HasRightToDrive;
+            Console.WriteLine($"{nameof(x)} has a value : {x.HasValue}");
+        }
+    }
+}
